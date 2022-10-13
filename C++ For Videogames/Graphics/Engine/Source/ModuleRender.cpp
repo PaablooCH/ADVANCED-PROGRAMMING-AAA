0552de7b0bfeefb9fad0303f50b673560d85bd14
Pablo Cebollada Hernández <62140600+PaablooCH@users.noreply.github.com>
2022-10-13 21:27:42 +0200

    graphic 1: background

--- C++ For Videogames/Graphics/Engine/Source/ModuleRender.cpp
+++ C++ For Videogames/Graphics/Engine/Source/ModuleRender.cpp
@@ -3,9 +3,17 @@
 #include "ModuleRender.h"
 #include "ModuleWindow.h"
 #include "SDL.h"
+#include "glew-2.1.0/include/GL/glew.h"
 
 ModuleRender::ModuleRender()
 {
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4); // desired version
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); // we want a double buffer
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24); // we want to have a depth buffer with 24 bits
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8); // we want to have a stencil buffer with 8 bits
+
 }
 
 // Destructor
@@ -16,16 +24,32 @@ ModuleRender::~ModuleRender()
 // Called before render is available
 bool ModuleRender::Init()
 {
+	SDL_GL_CreateContext(App->window->window);
+
 	LOG("Creating Renderer context");
 
-	//glewInit();
+	GLenum err = glewInit();
+	LOG("Using Glew %s", glewGetString(GLEW_VERSION));
 
+	LOG("Vendor: %s", glGetString(GL_VENDOR));
+	LOG("Renderer: %s", glGetString(GL_RENDERER));
+	LOG("OpenGL version supported %s", glGetString(GL_VERSION));
+	LOG("GLSL: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	glEnable(GL_DEPTH_TEST); // Enable depth test
+	glEnable(GL_CULL_FACE); // Enable cull backward faces
+	glFrontFace(GL_CCW); // Front faces will be counter clockwise
 
 	return true;
 }
 
 update_status ModuleRender::PreUpdate()
 {
+	int h, w;
+	SDL_GetWindowSize(App->window->window, &w, &h);
+	glViewport(0, 0, w, h);
+	glClearColor(1.f, 0.f, 0.f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	return UPDATE_CONTINUE;
 }
 
@@ -38,6 +62,7 @@ update_status ModuleRender::Update()
 
 update_status ModuleRender::PostUpdate()
 {
+	SDL_GL_SwapWindow(App->window->window);
 	return UPDATE_CONTINUE;
 }
 
@@ -46,7 +71,7 @@ bool ModuleRender::CleanUp()
 {
 	LOG("Destroying renderer");
 
-	//Destroy window
+	SDL_GL_DeleteContext(context);
 
 	return true;
 }
