1fc8a11080dcfbad2acf63d77b8ea990613271cc
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2022-11-14 00:41:57 +0100

    loadTexture function

--- Engine/Source/ModuleTexture.cpp
+++ Engine/Source/ModuleTexture.cpp
@@ -1,7 +1,17 @@
 #include "ModuleTexture.h"
-#include "DirectXTex/DirectXTex.h"
 
 bool ModuleTexture::Init()
 {
     return true;
+}
+
+void ModuleTexture::LoadTexture(const wchar_t* nameTexture, TexMetadata* metadata, ScratchImage& image)
+{
+    long success = LoadFromDDSFile(nameTexture, DDS_FLAGS_NONE, metadata, image);
+    if (success < 0) {
+        success = LoadFromTGAFile(nameTexture, metadata, image);
+        if (success < 0) {
+            LoadFromWICFile(nameTexture, WIC_FLAGS_NONE, metadata, image);
+        }
+    }
 }
--- Engine/Source/ModuleTexture.h
+++ Engine/Source/ModuleTexture.h
@@ -1,8 +1,12 @@
 #pragma once
 #include "Module.h"
+#include "DirectXTex/DirectXTex.h"
+using namespace DirectX;
+
 class ModuleTexture : public Module
 {
 public:
 	bool Init();
+	void LoadTexture(const wchar_t* nameTexture, TexMetadata* metadata, ScratchImage& image);
 };
 
